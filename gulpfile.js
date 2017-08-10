57613be6a43b6512b35440cc7794b342657b548b
alex <teos.nl@gmail.com>
2017-08-10 13:22:26 +0300

    add google map

---
+++ gulpfile.js
@@ -0,0 +1,213 @@
+const properties = {
+    folders: {
+      build: 'build',
+      src: 'src',
+    }
+}
+
+const plugins = {
+    js: [
+        'bower_components/jquery/dist/jquery.min.js',
+        'bower_components/slick-carousel/slick/slick.min.js',
+        'bower_components/jquery-mask-plugin/dist/jquery.mask.min.js',
+    ],
+    css: [
+        'bower_components/reset-css/reset.css',
+        'bower_components/slick-carousel/slick/slick.css',
+    ]
+}
+
+const gulp = require('gulp'),
+    connect = require('gulp-connect')
+    bs = require("browser-sync").create(),
+    sourcemaps = require('gulp-sourcemaps'),
+    concat = require('gulp-concat'),
+    pug = require('gulp-pug'),
+    sass = require('gulp-sass'),
+    prefix = require('gulp-autoprefixer')
+    babel = require('gulp-babel'),
+    rigger = require('gulp-rigger'),
+    watch = require('gulp-watch');
+    svgSprite = require('gulp-svg-sprite'),
+    svgmin = require('gulp-svgmin'),
+    cheerio = require('gulp-cheerio'),
+    replace = require('gulp-replace'),
+    typograf = require('gulp-typograf'),
+    eslint = require('gulp-eslint');
+
+function onError(err) {
+    console.log(err);
+    this.emit('end');
+}
+
+gulp.task('browserSync', function() {
+  bs.init({
+    server: {
+      baseDir: "./build"
+    },
+    //port: 8080,
+    open: true,
+    notify: false,
+    https: true
+  });
+});
+
+gulp.task('lint', function () {
+    return gulp.src(properties.folders.src + '/scripts/**/*.*')
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('scripts', function() {
+    return gulp.src([properties.folders.src + '/scripts/app.js'])
+        .pipe(sourcemaps.init())
+        .pipe(rigger())
+        .pipe(babel())
+        .on('error', onError)
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(properties.folders.build + '/scripts'))
+        .pipe(bs.stream());
+});
+
+gulp.task('vendor', function () {
+    gulp.src(plugins.css)
+        .pipe(concat('vendor.css'))
+        .pipe(gulp.dest(properties.folders.build + '/styles/'));
+    gulp.src(plugins.js)
+        .pipe(concat('vendor.js'))
+        .pipe(gulp.dest(properties.folders.build + '/scripts/'));
+});
+
+gulp.task('pug', function() {
+    gulp.src(properties.folders.src + '/views/pages/*.pug')
+          .pipe(pug({}))
+    .on('error', onError)
+    .pipe(gulp.dest(properties.folders.build))
+    .on('end', function(){
+        gulp.src(properties.folders.build + '/**/*.html')
+            .pipe(typograf({
+                locale: ['ru', 'en-US'],
+                enableRule: ["common/nbsp/afterNumber"],
+                disableRule: ["ru/other/phone-number"]
+            }))
+            .pipe(gulp.dest(properties.folders.build))
+            .pipe(bs.stream({once: true}));
+    });
+});
+
+gulp.task('sass', function () {
+    gulp.src(properties.folders.src + '/styles/main.scss')
+    .pipe(sourcemaps.init())
+		.pipe(sass.sync().on('error', sass.logError))
+		.pipe(prefix("last 3 version", "> 1%", "ie 8", "ie 7"))
+    .pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest(properties.folders.build + '/styles'))
+        .pipe(bs.stream());
+});
+
+gulp.task('image', function () {
+    gulp.src(properties.folders.src + '/img/**/*.*')
+        .pipe(gulp.dest(properties.folders.build + '/img'))
+});
+
+gulp.task('svgSpriteBuild', function () {
+	  return gulp.src(properties.folders.src + '/img/**/*.svg')
+    // minify svg
+    .pipe(svgmin({
+        js2svg: {
+            pretty: true
+        }
+    }))
+    // remove all fill, style and stroke declarations in out shapes
+    .pipe(cheerio({
+        run: function ($) {
+            $('[fill]').removeAttr('fill');
+            $('[stroke]').removeAttr('stroke');
+            $('[style]').removeAttr('style');
+        },
+        parserOptions: {xmlMode: true}
+    }))
+    // cheerio plugin create unnecessary string '&gt;', so replace it.
+    .pipe(replace('&gt;', '>'))
+    // build svg sprite
+    .pipe(svgSprite({
+        mode: {
+            symbol: {
+                sprite: "../sprite.svg",
+                render: {
+                    scss: {
+                        dest: '../../styles/_sprite.scss',
+                        template: properties.folders.src + "/styles/svg-templates/_sprite_template.scss"
+                    }
+                }
+            }
+        }
+    }))
+    .pipe(gulp.dest(properties.folders.src + '/sprite/'));
+});
+
+gulp.task('video', function () {
+    gulp.src(properties.folders.src + '/video/**/*.*')
+        .pipe(gulp.dest(properties.folders.build + '/video'))
+});
+
+gulp.task('font', function () {
+    gulp.src(properties.folders.src + '/fonts/**/*.*')
+        .pipe(gulp.dest(properties.folders.build + '/fonts'))
+});
+
+gulp.task('json', function () {
+    gulp.src(properties.folders.src + '/json/**/*.*')
+        .pipe(gulp.dest(properties.folders.build + '/json'))
+});
+
+gulp.task('server', function() {
+    connect.server({
+        port: properties.port,
+        root: properties.folders.build,
+        livereload: true
+    });
+});
+
+gulp.task('build', [
+    'pug',
+    'sass',
+    'scripts',
+    'lint',
+    'vendor',
+    'image',
+    'video',
+    'font',
+    'json',
+    'svgSpriteBuild'
+]);
+
+gulp.task('watch', function() {
+    watch(properties.folders.src + '/views/**/*.pug', function() {
+        gulp.start('pug');
+    });
+    watch(properties.folders.src + '/styles/**/*.scss', function() {
+        gulp.start('sass');
+    });
+    watch(properties.folders.src + '/scripts/**/*.js', function() {
+        gulp.start(['lint', 'scripts']);
+    });
+    watch(properties.folders.src + '/img/**/*.*', function() {
+        gulp.start('image');
+    });
+    watch(properties.folders.src + '/img/slide5/**/*.svg', function() {
+        gulp.start('svgSpriteBuild');
+    });
+    watch(properties.folders.src + '/video/**/*.*', function() {
+        gulp.start('video');
+    });
+    watch(properties.folders.src + '/font/**/*.*', function() {
+        gulp.start('font');
+    });
+    watch(properties.folders.src + '/json/**/*.*', function() {
+        gulp.start('json');
+    });
+});
+
+gulp.task('default', ['lint', 'build', 'browserSync', 'watch']);
---
+++ src/scripts/app.js
@@ -0,0 +1,283 @@
+/*global $ google MarkerWithLabel*/
+
+$(function() {
+
+	var BODY = document.body;
+
+    $(".js-sec4-gallery").slick({
+		slidesToShow: 2,
+		slidesToscroll: 2,
+		nextArrow: ".js-sec4-gallery-arrow-right",
+		prevArrow: ".js-sec4-gallery-arrow-left",
+		appendDots: ".sec4__gallery-dots",
+		dots: true,
+		infinite: false,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 1,
+					slidesToscroll: 1,
+				}
+			}
+		]
+	});
+
+	(function () {
+		var phoneValid = false;
+		var politicValid = false;
+
+		$("input[name=phone]").mask('+7 (000) 000-00-00', {
+
+			onKeyPress: function(cep, event, currentField, options){
+				checkPhone(cep, currentField);
+			}
+		});
+
+		$(".js-sec5-form").submit(function(e) {
+			e.preventDefault();
+
+			var self = $(this);
+			var phone = self.find("input[name=phone]");
+			var checkbox = self.find("input[type=checkbox]");
+
+			checkPhone(phone.val(), phone);
+			checkPolitic(checkbox)
+
+			if (phoneValid && politicValid) {
+				//console.log("form valid")
+			} else {
+				//console.log("form invalid")
+			}
+		});
+
+		$("input[type=checkbox]").on("change", function() {
+			var self = $(this);
+			checkPolitic(self);
+		});
+
+		function checkPhone(phone, field) {
+			
+			if (phone.length === 18) {
+				phoneValid = true;
+
+				if (field.hasClass("input_error")) {
+					field.removeClass("input_error");
+				}
+
+			} else {
+				phoneValid = false;
+				field.addClass("input_error");
+			}
+		}
+
+		function checkPolitic(checkbox) {
+			
+			if (checkbox.prop("checked")) {
+				politicValid = true;
+
+				if (checkbox.hasClass("checkbox_error")) {
+					checkbox.removeClass("checkbox_error");
+				}
+			} else {
+				politicValid = false;
+				checkbox.addClass("checkbox_error");
+			}
+		}
+	})();
+
+	
+	/*** google map ***/
+	function initMap() {
+
+		var markers = [];
+		var objects = [
+
+			{
+				name: "name1",
+				type: "main",
+				pos: [55.731594, 37.608650],
+				label: {
+					adress: "ул. Крымский Вал, 25",
+					phone: "+7 (777) 777-77-77"
+				}
+			},
+			{
+				name: "name2",
+				type: "default",
+				pos: [55.732173, 37.611405],
+				label: {
+					adress: "ул. Крымский Вал, 26",
+					phone: "+7 (777) 777-77-78"
+				}
+			},
+			{
+				name: "name3",
+				type: "default",
+				pos: [55.731381, 37.604727],
+				label: {
+					adress: "ул. Крымский Вал, 27",
+					phone: "+7 (777) 777-77-79"
+				}
+			}
+		];
+
+		var map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 17,
+			center: new google.maps.LatLng(objects[0].pos[0], objects[0].pos[1]),
+			disableDefaultUI: true,
+			scrollwheel: false,
+		});
+
+		for (var i = 0; i < objects.length; i++) {
+			addMarker(objects[i], map)
+		}
+
+		function addMarker(obj, map) {
+			
+			var marker = new MarkerWithLabel({
+				position: new google.maps.LatLng(obj.pos[0], obj.pos[1]),
+				map: map,
+				icon: {
+					url: '../img/default_map_mark.png',
+				},
+				labelClass: "sec6__map-popup",
+				labelAnchor: new google.maps.Point(100, 100),
+			});
+
+			var labelInfo = '<div><p>' + obj.label.adress + '</p><p>' + obj.label.phone + '</p></div>';
+
+			marker.addListener('click', function() {
+				map.setCenter(this.getPosition());
+
+				for (var i = 0; i < markers.length; i++) {
+					var item = markers[i];
+					
+					item.icon.url = "../img/default_map_mark.png";
+					item.setIcon("../img/default_map_mark.png");
+
+					item.labelContent = "";
+					item.label.setContent("");
+				}
+
+				this.setIcon("../img/main_map_mark.png");
+				this.labelContent = labelInfo;
+				this.label.setContent(labelInfo);
+			});
+
+			markers.push(marker);
+		}
+
+		var clusterStyles = [
+
+			{
+				textColor: 'white',
+				url: '../img/default_map_mark.png',
+				height: 50,
+				width: 50
+			},
+			{
+				textColor: 'white',
+				url: '../img/default_map_mark.png',
+				height: 50,
+				width: 50
+			},
+			{
+				textColor: 'white',
+				url: '../img/default_map_mark.png',
+				height: 50,
+				width: 50
+			}
+		];
+
+		var markerCluster = new MarkerClusterer(map, markers, {styles: clusterStyles});
+	}
+
+	initMap();
+	/*** /google map ***/
+
+
+	/*** modal ****/
+
+	function openModal(modal) {
+		var modalEl = document.querySelector(modal);
+		modalEl.classList.add("modal_show");
+		BODY.classList.add("body_overflow");
+	}
+
+	function closeModal() {
+		var openModal = document.querySelector(".modal_show");
+		openModal.classList.remove("modal_show");
+		BODY.classList.remove("body_overflow");
+	}
+
+	document.querySelector(".header__logo").addEventListener("click", function(e) {
+		e.preventDefault();
+		openModal(".action__modal");
+	});
+
+	/*** nav bar on mobile ***/
+		function openNavBar() {
+			var navBar = document.querySelector(".header__nav-list");
+			navBar.classList.add("header__nav-list_show");
+			BODY.classList.add("body_overflow");
+		}
+
+		function closeNavBar() {
+			var openNavBar = document.querySelector(".header__nav-list_show");
+			openNavBar.classList.remove("header__nav-list_show");
+			BODY.classList.remove("body_overflow");
+		}
+
+		document.querySelector(".header__nav-menu").addEventListener("click", function() {
+			openNavBar();
+		});
+	/*** /nav bar on mobile ***/
+
+
+	/*** header link click ****/
+	$(".header__nav-link").on("click", function(e) {
+		e.preventDefault();
+
+		var self = $(this);
+		var target = self.attr("href");
+		var noteHeight = 0;
+
+		if ($(target).find(".note").length > 0) {
+			var note = $(target).find(".note");
+			noteHeight = note.outerHeight();
+		}
+
+		$("html, body").animate({
+			scrollTop: $(target).offset().top - noteHeight / 2
+		}, 600, function() {
+
+			if (document.querySelector(".header__nav-list").classList.contains("header__nav-list_show")) {
+				closeNavBar();
+			}
+		});
+	});
+	/*** /header link click ****/
+
+	/*** BODY click handler ****/
+	BODY.addEventListener("click", function(e) {
+
+		if (e.target.classList.contains("modal") || e.target.classList.contains("modal__close")) {
+			closeModal();
+		}
+
+		if (e.target.classList.contains("header__nav-close")) {
+			closeNavBar();
+		}
+	});
+	/*** BODY click handler ****/
+
+	window.addEventListener("resize", function() {
+
+		if (document.querySelector(".header__nav-list_show")) {
+			closeNavBar();
+		}
+			
+	})
+});
+
